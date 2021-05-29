3f11ad7f18d28e1c8cbe9ef093f3c1c94a174a30
finegs <finegs@gmail.com>
2021-05-30 07:26:46 +0900

    start kafka one batch

---
+++ start_consume_produce.bat
@@ -0,0 +1,8 @@
+rem change log path to using path
+start bin\windows\zookeeper-server-start.bat config\zookeeper.properties
+rem change log path to using path
+start bin\windows\kafka-server-start.bat config\server.properties
+bin\windows\kafka-topics.bat --bootstrap-server localhost:9092 --create --topic my-test-topic --replication-factor 1 --partitions 3 
+bin\windows\kafka-topics.bat --bootstrap-server localhost:9092 --describe --topic my-test-topic  
+start bin\windows\kafka-console-consumer.bat --topic my-test-topic --from-beginning --bootstrap-server localhost:9092 
+start bin\windows\kafka-console-producer.bat --topic my-test-topic --bootstrap-server localhost:9092 
