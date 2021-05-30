733e6157b1fd707671595de125967623f53241fb
finegs <finegs@gmail.com>
2021-05-30 09:29:48 +0900

    stop zookeeper, kafka server

--- start_consume_produce.bat
+++ start_consume_produce.bat
@@ -1,4 +1,5 @@
 rem change log path to using path
+cd /d %KAFKA_HOME%
 start bin\windows\zookeeper-server-start.bat config\zookeeper.properties
 rem change log path to using path
 start bin\windows\kafka-server-start.bat config\server.properties
@@ -6,3 +7,7 @@ bin\windows\kafka-topics.bat --bootstrap-server localhost:9092 --create --topic 
 bin\windows\kafka-topics.bat --bootstrap-server localhost:9092 --describe --topic my-test-topic  
 start bin\windows\kafka-console-consumer.bat --topic my-test-topic --from-beginning --bootstrap-server localhost:9092 
 start bin\windows\kafka-console-producer.bat --topic my-test-topic --bootstrap-server localhost:9092 
+
+rem stop zookeeper, kafka broker
+start bin\windows\kafka-server-stop.bat
+start bin\windows\zookeeper-server-stop.bat
