f42846aa8206787362f7886ed4ef45f2dbc2a7a4
KANGSOONGOO <finegs@gmail.com>
2022-07-09 09:35:28 +0900

    build batch for paho-mqtt.c/cpp

---
+++ build_mqtt3c.bat
@@ -0,0 +1,7 @@
+@REM Remove build folder
+rd /s /q build 
+@REM Generate CMakeFiles
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug -DPAHO_WITH_SSL=ON -DPAHO_BUILD_SHARED=ON -DPAHO_BUILD_STATIC=ON -DPAHO_BUILD_SAMPLES=ON -DPAHO_HIGH_PERFORMANCE=ON -DPAHO_USE_SELECT=OFF -DCMAKE_INSTALL_PREFIX=D:\\Programs\\share -DOPENSSL_ROOT_DIR=D:\\Programs\\share
+@REM Build
+cmake --build build --target ALL_BUILD -- /t:Rebuild /m:8
+cmake --build build --target install
