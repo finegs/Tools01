218a535f536fca82dd9df86cc2bbcbd9b49de736
finegs <finegs@gmail.com>
2025-09-14 22:29:46 +0900

    for fixing vulnerablity

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.root_spec = { "lsp", { ".pjt.vim", ".init.lua", "lua" }, "cwd" }
+vim.g.root_spec = { "lsp", { ".pjt.lua", ".init.lua", "lua" }, "cwd" }
 
 vim.g.root_lsp_ignore = { ".gitignore" }
 
