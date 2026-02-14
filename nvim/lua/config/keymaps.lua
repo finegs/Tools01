6cd09544d2a9c15ff00d8961eac75b3b6283edaa
finegs <finegs@gmail.com>
2026-02-14 22:28:28 +0900

    new[nvim-plugin] - snacks with var_viewer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,3 +19,20 @@ vim.api.nvim_set_keymap(
 vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { remap = true, desc = "Next Buffer Cycle" })
 vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", { remap = true, desc = "Prev Buffer Cycle" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { remap = true, desc = "Delete Current Buffer " })
+
+-- Session
+-- 1. Save Session (Overwrite .session.vim)
+vim.keymap.set("n", "<leader>ms", function()
+  vim.cmd("mksession! .session.vim")
+  vim.notify("Session saved to .session.vim", vim.log.levels.INFO)
+end, { desc = "Save Session (Overwrite)" })
+
+-- 2. Load Session (Check if exists first)
+vim.keymap.set("n", "<leader>ml", function()
+  if vim.fn.filereadable(".session.vim") == 1 then
+    vim.cmd("source .session.vim")
+    vim.notify("Session loaded", vim.log.levels.INFO)
+  else
+    vim.notify("No .session.vim file found", vim.log.levels.WARN)
+  end
+end, { desc = "Load Session" })
