e9a86e9b34d7bd3087d9a114a3d4723ca48e1015
finegs <finegs@gmail.com>
2026-06-13 16:37:47 +0900

    chore(CMakeLists.txt): add commpile command on

--- cpjts/aa/aa.cpp
+++ cpjts/aa/aa.cpp
@@ -1,10 +1,12 @@
 
-#include<cstdio>
+#include <cstdio>
 
 int main() {
-	int a,b;
-	printf("aais good\n");
-	scanf("%d%d", &a, &b);	
-	printf("sum(%d, %d)=%d\n", a,b,a+b);
-	return 0;
+  int a, b;
+  printf("aa is good\n");
+  printf("a b : ");
+  fflush(stdout);
+  scanf("%d%d", &a, &b);
+  printf("sum(%d, %d)=%d\n", a, b, a + b);
+  return 0;
 }
